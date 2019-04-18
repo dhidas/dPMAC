2c3ff95b302e051f10d49b712be9137998d07b0c
Dean Andrew Hidas <dhidas@bnl.gov>
2019-04-18 08:42:07 -0400

    save and reboot exes

---
+++ exe/dpsave.cc
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////
+//
+// Dean Andrew Hidas <dhidas@bnl.gov>
+//
+// Created on: Mon Jul 23 09:53:54 EDT 2018
+//
+////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+
+#include "PMAC2Turbo.h"
+
+
+int dpsave (std::string const& IP)
+{
+  PMAC2Turbo PMAC(IP, 1025);
+  PMAC.SendLine("save");
+
+  return 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+  std::cout << "Will: $$$***, SAVE, $$$, download file, SAVE, and $$$" << std::endl;
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " [IP]" << std::endl;
+    return 1;
+  }
+
+  dpsave(argv[1]);
+
+  return 0;
+}
