ade1db8c762a059936022f362c326d018ae61197
Stuart B. Wilkins <swilkins@bnl.gov>
2020-10-03 10:46:07 -0400

    Added simple IDENT routine

---
+++ exe/dpident.cc
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////
+//
+// Stuart B. Wilkins <swilkins@bnl.gov>
+//
+// Created on : 10/03/2020 10:03:21 AM
+//
+////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <unistd.h>
+
+#include "PMAC2Turbo.h"
+
+
+int dpident (std::string const& IP)
+{
+
+  PMAC2Turbo PMAC(IP, 1025);
+
+  PMAC.SendLine("IDNUMBER");
+  sleep(5);
+  PMAC.GetBuffer();
+
+  return 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " [IP]" << std::endl;
+    return 1;
+  }
+
+  dpident(argv[1]);
+
+  return 0;
+}
